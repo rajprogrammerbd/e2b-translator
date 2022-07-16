259318e0c7fb37c331de1059954beed123013be8
Raj Dutta <rd2249619@gmail.com>
2022-07-16 13:45:08 +0600

    fix the header component getting data from redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Header from './components/Header';
+import { RootState } from './redux/store';
 
 function App() {
+  const isLogin = useSelector((state: RootState) => state.login.isLogin);
 
   return (
     <>
-      <Header isLogin={false} />
+      <Header isLogin={isLogin} />
       <div className="md:container mx-auto">
         <small>Hello world</small>
       </div>    
