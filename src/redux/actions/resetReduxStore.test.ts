8358f21ba52f99111782c630f41d203fb81d8c50
Raj Dutta <rd2249619@gmail.com>
2022-08-01 23:00:34 +0600

    add demo comment

--- src/redux/actions/resetReduxStore.test.ts
+++ src/redux/actions/resetReduxStore.test.ts
@@ -3,7 +3,7 @@ import resetReduxStore from './resetReduxStore';
 
 test('resetReduxStore function returns object', () => {
     const value = resetReduxStore();
-
+    console.log('demo comment');
     expect(value).toMatchObject({
         type: 'REST_REDUX_STORE',
       });
