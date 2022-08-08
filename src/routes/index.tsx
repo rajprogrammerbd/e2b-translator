2cfc6ad21e8f1cbfd35b8fba84a93b3c13fc5056
Raj Dutta <rd2249619@gmail.com>
2022-08-08 12:34:44 +0600

    fix react-router-dom redirection issue

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "../pages/Homepage";
 import LoginPage from "../pages/Loginpage";
 import ErrorPage from '../pages/ErrorPage';
@@ -9,13 +9,11 @@ function RouteApp() {
     const isLogin = useSelector((state: RootState) => state.login.isLogin);
 
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/login" element={isLogin ? <HomePage /> : <LoginPage />} />
-                <Route path="/*" element={<ErrorPage />} />
-            </Routes>
-        </BrowserRouter>
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={isLogin ? <HomePage /> : <LoginPage />} />
+            <Route path="/*" element={<ErrorPage />} />
+        </Routes>
     );
 }
 
