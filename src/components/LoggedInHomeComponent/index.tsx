03b5aa885e2025794ad50f6dec2624aa1ece65b2
Raj Dutta <rd2249619@gmail.com>
2022-09-29 23:19:23 +0600

    remove unused useState import

--- src/components/LoggedInHomeComponent/index.tsx
+++ src/components/LoggedInHomeComponent/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { displayedAnimation } from '../../redux/slice/login';
 import { RootState } from '../../redux/store';
